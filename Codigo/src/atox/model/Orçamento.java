3a1dca30a7ec9bb42925d404b6dc886aa76e26bc
Henrique Lavieri <hlfacio@hotmail.com>
2018-10-10 14:25:53 -0300

    algumas classes

---
+++ Codigo/src/atox/model/Orçamento.java
@@ -0,0 +1,19 @@
+package atox.model;
+
+public class Orçamento {
+
+    private int codOrcamento;
+    private String placaCarro;
+    private Pagamento pagamento;
+    private Date dataHora;
+    private String statusAtual;
+
+    public void Orcamento() {
+
+    }
+    public void setOrcamento(Orcamento){
+
+    }
+
+
+}
